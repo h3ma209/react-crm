825133889416fc58ded25d0282ba66328de03e0b
hema omer <32086055+big-yoshi@users.noreply.github.com>
2022-01-05 21:44:50 +0300

    added navbar and login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from './Login/index.js'
+import Navbar from "./Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    
     <Router>
       <div className="App">
-
+      <Navbar></Navbar>
         <Routes>
         <Route path='/login' element={<Login/>} />
           
